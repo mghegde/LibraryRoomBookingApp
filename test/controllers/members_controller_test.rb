789ba22da589ab8770a15c5f71f7a6a3e86b5972
sneha2212 <snehadesh@gmail.com>
2016-10-05 19:04:49 -0400

    Added testcases for member controller

--- test/controllers/members_controller_test.rb
+++ test/controllers/members_controller_test.rb
@@ -5,23 +5,14 @@ class MembersControllerTest < ActionController::TestCase
     @member = members(:one)
   end
 
-  test "should get index" do
-    get :index
-    assert_response :success
-    assert_not_nil assigns(:members)
-  end
-
   test "should get new" do
     get :new
     assert_response :success
   end
 
   test "should create member" do
-    assert_difference('Member.count') do
-      post :create, member: { email: @member.email, name: @member.name, password: @member.password }
-    end
-
-    assert_redirected_to member_path(assigns(:member))
+    post :create, member: { email: @member.email, name: @member.name, password: @member.password }
+    assert_response :success
   end
 
   test "should show member" do
@@ -36,14 +27,11 @@ class MembersControllerTest < ActionController::TestCase
 
   test "should update member" do
     patch :update, id: @member, member: { email: @member.email, name: @member.name, password: @member.password }
-    assert_redirected_to member_path(assigns(:member))
+    assert_response :success
   end
 
   test "should destroy member" do
-    assert_difference('Member.count', -1) do
-      delete :destroy, id: @member
-    end
-
-    assert_redirected_to members_path
+    delete :destroy, id: @member
+    assert_response :success
   end
 end
